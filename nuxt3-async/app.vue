ba4335a786ad14d4ad94b2c6da07d98fff61b327
ハン・キョンス <hankyonsu@hankyonsunoMacBook-Air.local>
2023-08-03 23:46:10 +0900

    非同期処理練習

---
+++ nuxt3-async/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="user in users" :key = "user.id">
+        {{ user.id }}, {{ user.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script setup>
+  const {
+    data: users, error
+  } = await useFetch('https://jsonplaceholder.typicode.com/userss')
+  // console.log(error.value)
+  if(error.value){
+    throw createError({statusCode: 404, statusMessage :'Page not found!!!!'})
+  }
+</script>
