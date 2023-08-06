bd246969fe36e07d6c21745c8dfac73bd336b3a8
ハン・キョンス <hankyonsu@hankyonsunoMacBook-Air.local>
2023-08-06 13:46:37 +0900

    todoリスト完了

--- nuxt3-todo/app.vue
+++ nuxt3-todo/app.vue
@@ -6,12 +6,12 @@
       <button>Add</button>
     </form>
     <ul>
-      <li v-for="(task) in tasks" :key="task">
+      <li v-for="(task, index) in tasks" :key="task">
         <span>{{ task }}</span>
-        <button>Delete</button>
+        <button @click="$event => deleteTask(index)">Delete</button>
       </li>
     </ul>
-    <button>Clear</button>
+    <button @click="clearTask">Clear</button>
   </div>
 </template>
 <script setup>
@@ -28,5 +28,13 @@
       tasks.value.push(newTask.value)
     }
     newTask.value = ''
+  }
+
+  function deleteTask(index) {
+    tasks.value.splice(index, 1)
+  }
+
+  function clearTask() {
+    tasks.value = [];
   }
 </script>
